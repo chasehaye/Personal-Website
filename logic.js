d6cdce6cab29d985360bca1616576cacd5e231a9
chasehaye <chasehayejoc@gmail.com>
2023-04-03 18:12:06 -0700

    v1.2 finished nav bar structure

--- logic.js
+++ logic.js
@@ -7,10 +7,11 @@ const linksMenuBtnMob = document.querySelector(".link-btn-mob");
 const linksMenuBtnDesk = document.querySelector(".link-btn-desk");
 const linksMenuMob = document.querySelectorAll(".subMob")
 const linksMenuDesk = document.querySelectorAll(".subDesk")
-
+const subDrop = document.querySelector(".sub-drop")
 // open and close the two sub menu variations based on screen size
 function togSubMenus() {
     let wid = window.innerWidth;
+        linkMenuOff();
     if ( wid <= 991) {
      mobileSubMenu.classList.toggle("hidden");
      title.classList.remove("hidden");
@@ -25,24 +26,31 @@ function togSubMenus() {
      });
     }
 }
+// close link sub menus
+function linkMenuOff() {
+    subDrop.classList.add("hidden");
+    linksMenuMob.forEach((component) => {
+        component.classList.add("hidden")
+    });
+    linksMenuDesk.forEach((component) => {
+        component.classList.add("hidden")
+    });
+}
 // toggles mobile submenu links
 function togLinksMenuMob() {
+    subDrop.classList.toggle("hidden");
     linksMenuMob.forEach((component) => {
         component.classList.toggle("hidden")
     });
 }
-
 // toggles desktop submenu
 function togLinksMenuDesk() {
+    subDrop.classList.toggle("hidden");
     linksMenuDesk.forEach((component) => {
         component.classList.toggle("hidden")
     });
 }
-
-
-
-
-//listner to close sub menus if the browser resizes
+// listner to close sub menus if the browser resizes
 addEventListener("resize", (e) => {
     title.classList.remove("hidden");
     mobileSubMenu.classList.add("hidden");
